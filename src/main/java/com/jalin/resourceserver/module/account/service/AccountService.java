74c829359b4d5c6abf3e34d56f12608d59db667f
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-06 22:35:40 +0700

    Add get account transactions service

--- src/main/java/com/jalin/resourceserver/module/account/service/AccountService.java
+++ src/main/java/com/jalin/resourceserver/module/account/service/AccountService.java
@@ -2,10 +2,13 @@ package com.jalin.resourceserver.module.account.service;
 
 import com.jalin.resourceserver.module.account.entity.Account;
 import com.jalin.resourceserver.module.account.model.AccountDto;
+import com.jalin.resourceserver.module.account.model.TransactionDto;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface AccountService {
     void addNewAccount(UUID customerId, Account requestBody);
     AccountDto getAccountByAccountNumber(String accountNumber);
+    Set<TransactionDto> getTransactionsByAccountNumber(String accountNumber);
 }
--- src/main/java/com/jalin/resourceserver/module/account/service/AccountServiceImpl.java
+++ src/main/java/com/jalin/resourceserver/module/account/service/AccountServiceImpl.java
@@ -3,23 +3,34 @@ package com.jalin.resourceserver.module.account.service;
 import com.jalin.resourceserver.exception.ResourceNotFoundException;
 import com.jalin.resourceserver.module.account.entity.Account;
 import com.jalin.resourceserver.module.account.entity.Customer;
+import com.jalin.resourceserver.module.account.entity.Transaction;
 import com.jalin.resourceserver.module.account.model.AccountDto;
+import com.jalin.resourceserver.module.account.model.TransactionDto;
 import com.jalin.resourceserver.module.account.repository.AccountRepository;
 import com.jalin.resourceserver.module.account.repository.CustomerRepository;
+import com.jalin.resourceserver.module.account.repository.TransactionRepository;
+import com.jalin.resourceserver.utility.DateGeneratorUtility;
 import com.jalin.resourceserver.utility.ModelMapperUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private DateGeneratorUtility dateGeneratorUtility;
     @Autowired
     private ModelMapperUtility modelMapperUtility;
     @Autowired
     private AccountRepository accountRepository;
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @Override
     public void addNewAccount(UUID customerId, Account requestBody) {
@@ -40,5 +51,22 @@ public class AccountServiceImpl implements AccountService {
                 .orElseThrow(() -> new ResourceNotFoundException(
                         String.format("Account with account number %s not found", accountNumber)));
         return modelMapperUtility.mapper().map(account, AccountDto.class);
+    }
+
+    @Override
+    public Set<TransactionDto> getTransactionsByAccountNumber(String accountNumber) {
+        Account account = accountRepository.findById(accountNumber)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        String.format("Account with account number %s not found", accountNumber)));
+
+        List<Transaction> transactionList = transactionRepository.findByAccountAndTransactionDate(
+                account,
+                dateGeneratorUtility.generateDate());
+        Set<TransactionDto> transactionDtoSet = new HashSet<>();
+        for (Transaction transaction : transactionList) {
+            TransactionDto transactionDto = modelMapperUtility.mapper().map(transaction, TransactionDto.class);
+            transactionDtoSet.add(transactionDto);
+        }
+        return transactionDtoSet;
     }
 }
