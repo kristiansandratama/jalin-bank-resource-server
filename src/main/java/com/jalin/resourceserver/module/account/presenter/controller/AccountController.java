c4b49f7bb6ea831c001670bb530cb56a9f3a6eeb
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-06 22:36:11 +0700

    Add get transactions by account number endpoint

--- src/main/java/com/jalin/resourceserver/module/account/presenter/controller/AccountController.java
+++ src/main/java/com/jalin/resourceserver/module/account/presenter/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.jalin.resourceserver.model.SuccessDetailsResponse;
 import com.jalin.resourceserver.model.SuccessResponse;
 import com.jalin.resourceserver.module.account.entity.Account;
 import com.jalin.resourceserver.module.account.model.AccountDto;
+import com.jalin.resourceserver.module.account.model.TransactionDto;
 import com.jalin.resourceserver.module.account.presenter.model.AddNewAccountRequest;
 import com.jalin.resourceserver.module.account.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
@@ -35,6 +37,14 @@ public class AccountController {
         AccountDto accountDto = accountService.getAccountByAccountNumber(accountNumber);
         return new ResponseEntity<>(
                 new SuccessDetailsResponse(true, "Bank account successfully found", accountDto),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/accounts/{accountNumber}/transactions")
+    public ResponseEntity<Object> getTransactionsByAccountNumber(@PathVariable String accountNumber) {
+        Set<TransactionDto> transactionDtoSet = accountService.getTransactionsByAccountNumber(accountNumber);
+        return new ResponseEntity<>(
+                new SuccessDetailsResponse(true, "Transactions successfully found", transactionDtoSet),
                 HttpStatus.OK);
     }
 }
