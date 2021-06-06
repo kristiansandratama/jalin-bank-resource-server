d517c12f1bdc680a1b401b46dcfcbadf8a7b84ed
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-06 21:21:43 +0700

    Refactor account repository

--- src/main/java/com/jalin/resourceserver/module/account/repository/AccountRepository.java
+++ src/main/java/com/jalin/resourceserver/module/account/repository/AccountRepository.java
@@ -1,13 +1,9 @@
 package com.jalin.resourceserver.module.account.repository;
 
 import com.jalin.resourceserver.module.account.entity.Account;
-import com.jalin.resourceserver.module.account.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
-    List<Account> findByCustomer(Customer customer);
 }
