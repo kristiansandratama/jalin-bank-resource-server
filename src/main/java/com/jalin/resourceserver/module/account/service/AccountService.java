e81c34e838897533cef0893877b51f184cc564af
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-06 13:40:36 +0700

    Add get account balance service and endpoint

--- src/main/java/com/jalin/resourceserver/module/account/service/AccountService.java
+++ src/main/java/com/jalin/resourceserver/module/account/service/AccountService.java
@@ -1,9 +1,11 @@
 package com.jalin.resourceserver.module.account.service;
 
 import com.jalin.resourceserver.module.account.entity.Account;
+import com.jalin.resourceserver.module.account.model.AccountDto;
 
 import java.util.UUID;
 
 public interface AccountService {
     void addNewAccount(UUID customerId, Account requestBody);
+    AccountDto getByAccountNumber(String accountNumber);
 }
