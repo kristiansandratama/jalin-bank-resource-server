0a029cf8e9d94d4cfeb988a169d427d8d14fc7c0
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-22 19:27:35 +0700

    Add fund transfer domestic request model

---
+++ src/main/java/com/jalin/resourceserver/module/dummy/presenter/model/FundTransferDomesticRequest.java
@@ -0,0 +1,25 @@
+package com.jalin.resourceserver.module.dummy.presenter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class FundTransferDomesticRequest {
+    @NotBlank
+    private String sourceAccountNumber;
+    @NotBlank
+    private String corporateId;
+    @NotBlank
+    private String beneficiaryAccountNumber;
+    @Positive
+    private BigDecimal amount;
+}
