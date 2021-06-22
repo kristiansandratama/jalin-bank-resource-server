ce1b8d5d03c435f7dfddc924a088f8bb4313fd9a
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-22 19:23:24 +0700

    Change transactionDate argument data type from String to LocalDate

--- src/main/java/com/jalin/resourceserver/module/account/repository/TransactionRepository.java
+++ src/main/java/com/jalin/resourceserver/module/account/repository/TransactionRepository.java
@@ -5,11 +5,12 @@ import com.jalin.resourceserver.module.account.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
     @Query("SELECT t FROM Transaction t WHERE t.account = :account " +
             "AND t.transactionDate = :transactionDate " +
             "ORDER BY t.createdDate ASC")
-    List<Transaction> findByAccountAndTransactionDate(Account account, String transactionDate);
+    List<Transaction> findByAccountAndTransactionDate(Account account, LocalDate transactionDate);
 }
