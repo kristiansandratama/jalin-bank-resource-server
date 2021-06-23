03856711d10cdec3970fa9fb278cfaf1140abd50
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-23 12:29:32 +0700

    Add find corporates by type with order by corporate name

--- src/main/java/com/jalin/resourceserver/module/dummy/repository/CorporateRepository.java
+++ src/main/java/com/jalin/resourceserver/module/dummy/repository/CorporateRepository.java
@@ -1,9 +1,13 @@
 package com.jalin.resourceserver.module.dummy.repository;
 
 import com.jalin.resourceserver.module.dummy.entity.Corporate;
+import com.jalin.resourceserver.module.dummy.entity.CorporateTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CorporateRepository extends JpaRepository<Corporate, String> {
+    List<Corporate> findByCorporateTypeOrderByCorporateNameAsc(CorporateTypeEnum corporateType);
 }
