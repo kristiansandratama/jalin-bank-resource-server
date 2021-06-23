0f454fa3a20eb4c1bccd5c8987a8e795df2f252a
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-23 12:29:54 +0700

    Add get corporates endpoints

---
+++ src/main/java/com/jalin/resourceserver/module/dummy/presenter/controller/CorporateController.java
@@ -0,0 +1,36 @@
+package com.jalin.resourceserver.module.dummy.presenter.controller;
+
+import com.jalin.resourceserver.model.SuccessDetailsResponse;
+import com.jalin.resourceserver.module.dummy.model.CorporateDto;
+import com.jalin.resourceserver.module.dummy.service.CorporateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CorporateController {
+    @Autowired
+    private CorporateService corporateService;
+
+    @GetMapping("/corporates/bank")
+    public ResponseEntity<Object> getBankCorporates() {
+        List<CorporateDto> corporateDtoList = corporateService.getBankCorporates();
+        return new ResponseEntity<>(
+                new SuccessDetailsResponse(true, "Bank corporates successfully found", corporateDtoList),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/corporates/wallet")
+    public ResponseEntity<Object> getDigitalWalletCorporates() {
+        List<CorporateDto> corporateDtoList = corporateService.getDigitalWalletCorporates();
+        return new ResponseEntity<>(
+                new SuccessDetailsResponse(true, "Digital wallet corporates successfully found", corporateDtoList),
+                HttpStatus.OK);
+    }
+}
