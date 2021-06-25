931b8c7211beab93c3cf6db048a23fd07192c058
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-25 17:23:09 +0700

    Add find by account number query

--- src/main/java/com/jalin/resourceserver/module/account/repository/AccountRepository.java
+++ src/main/java/com/jalin/resourceserver/module/account/repository/AccountRepository.java
@@ -4,6 +4,9 @@ import com.jalin.resourceserver.module.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
